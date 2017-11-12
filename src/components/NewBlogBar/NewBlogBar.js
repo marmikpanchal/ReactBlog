72ef4495e529a931d3e1cb89279d99879c870a6b
Marmik Panchal <marmik.panchal@gmail.com>
2017-11-12 23:40:01 +1100

    Working on input blog box

--- src/components/NewBlogBar/NewBlogBar.js
+++ src/components/NewBlogBar/NewBlogBar.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import Button from 'muicss/lib/react/button';
+import Textarea from "react-textarea-autosize";
 import './NewBlogBar.scss'
 
 class NewBlogBar extends React.Component {
+
+	// onSubmit(ev) {
+	//     ev.preventDefault();  // prevent form submission
+	//     alert(this.input.controlEl.value);
+	// }
+
+	// render(){
+	// 	return (
+	// 		<div className='newblog'>
+	// 			<form className='height' onSubmit={this.onSubmit.bind(this)}>
+ //        			<Textarea ref={el => { this.input = el; }} defaultValue="Value on load" />    
+	// 				<Button className='hello' size="large" color="primary">Write a new Blog</Button>
+	// 			</form>
+	// 		</div>
+	// 	);
+	// }
+
 	render(){
 		return (
-			<div className='newblog'>
-				<Button className='hello' size="large" color="primary">Write a new Blog</Button>
+			<div className='newblog'> 
+				<Textarea className='textarea'/>
+				<Button className='btn' size="large" color="primary">Post a new Blog</Button>
 			</div>
 		);
 	}
